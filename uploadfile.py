4e89e78e1a0284b9c9d7563c2842861eb6ba79c5
sg3510 <sg3510@ic.ac.uk>
2014-07-15 13:07:41 +0100

    Upload

--- uploadfile.py
+++ uploadfile.py
@@ -1,7 +1,5 @@
 import requests
 
-# r = requests.post('http://localhost:8888/upload', files={'test.txt': open('test.txt', 'rb')})
-
 url = 'http://localhost:8888/upload'
 files = {'filearg': open('alert01.wav', 'rb')}
 
