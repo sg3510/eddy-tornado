d1d8994bac95b8ff955b01621dd89fda90f7f605
sazerzac <me@zackmitchell.com>
2014-07-15 13:10:21 +0100

    file as argument

--- uploadfile.py
+++ uploadfile.py
@@ -1,7 +1,10 @@
-import requests
+import requests, sys
 
+
+file_name = sys.argv
+print file_name[1]
 url = 'http://localhost:8888/upload'
-files = {'filearg': open('alert01.wav', 'rb')}
+files = {'filearg': open(file_name[1], 'rb')}
 
 r = requests.post(url, files=files)
 r.text
