75953611233a62341210fb644295968ef992a08d
sg3510 <sg3510@ic.ac.uk>
2014-07-15 13:04:39 +0100

    Send file of choice

--- uploadfile.py
+++ uploadfile.py
@@ -3,7 +3,7 @@ import requests
 # r = requests.post('http://localhost:8888/upload', files={'test.txt': open('test.txt', 'rb')})
 
 url = 'http://localhost:8888/upload'
-files = {'filearg': ('report.csv', 'some,data,to,send\nanother,row,to,send\n')}
+files = {'filearg': open('alert01.wav', 'rb')}
 
 r = requests.post(url, files=files)
 r.text
