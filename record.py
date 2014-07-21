4a8a914b6d8ca5f91a83e598ec9c49e28cf7825d
sazerzac <me@zackmitchell.com>
2014-07-21 17:24:23 +0100

    Audio trigger and multiple file script

--- record.py
+++ record.py
@@ -1,38 +1,99 @@
 import pyaudio
 import wave
+import audioop
+from collections import deque
+import os
+import time
+import math
 
-CHUNK = 1024 
-FORMAT = pyaudio.paInt16 #paInt8
-CHANNELS = 2 
-RATE = 44100 #sample rate
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+# Microphone stream config.
+CHUNK = 1024  # CHUNKS of bytes to read each time from mic
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 44100
+THRESHOLD = 2500  # The threshold intensity that defines silence
+                  # and noise signal (an int. lower than THRESHOLD is silence).
 
-p = pyaudio.PyAudio()
+SILENCE_LIMIT = 1  # Silence limit in seconds. The max ammount of seconds where
+                   # only silence is recorded. When this time passes the
+                   # recording finishes and the file is delivered.
 
-stream = p.open(format=FORMAT,
-                channels=CHANNELS,
-                rate=RATE,
-                input=True,
-                frames_per_buffer=CHUNK) #buffer
+PREV_AUDIO = 1  # Previous audio (in seconds) to prepend. When noise
+                  # is detected, how much of previously recorded audio is
+                  # prepended. This helps to prevent chopping the beggining
+                  # of the phrase.
 
-print("* recording")
+def listen_for_speech(threshold=THRESHOLD, num_phrases=-1):
 
-frames = []
+    #Open stream
+    p = pyaudio.PyAudio()
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    data = stream.read(CHUNK)
-    frames.append(data) # 2 bytes(16 bits) per channel
+    stream = p.open(format=FORMAT,
+                    channels=CHANNELS,
+                    rate=RATE,
+                    input=True,
+                    frames_per_buffer=CHUNK)
 
-print("* done recording")
+    print "* Listening mic. "
+    audio2send = []
+    cur_data = ''  # current chunk  of audio data
+    rel = RATE/CHUNK
+    slid_win = deque(maxlen=SILENCE_LIMIT * rel)
+    #Prepend audio from 0.5 seconds before noise was detected
+    prev_audio = deque(maxlen=PREV_AUDIO * rel) 
+    started = False
+    n = num_phrases
+    response = []
 
-stream.stop_stream()
-stream.close()
-p.terminate()
+    while (num_phrases == -1 or n > 0):
+        cur_data = stream.read(CHUNK)
+        slid_win.append(math.sqrt(abs(audioop.avg(cur_data, 4))))
+        #print slid_win[-1]
+        if(sum([x > THRESHOLD for x in slid_win]) > 0):
+            if(not started):
+                print "Starting record of phrase"
+                started = True
+            audio2send.append(cur_data)
+        elif (started is True):
+            print "Finished"
+            # The limit was reached, finish capture and deliver.
+            filename = save_speech(list(prev_audio) + audio2send, p)
+            
+            if num_phrases == -1:
+                print "Caught 1" #, r
+            
+            # Reset all
+            started = False
+            slid_win = deque(maxlen=SILENCE_LIMIT * rel)
+            prev_audio = deque(maxlen=0.5 * rel) 
+            audio2send = []
+            n -= 1
+            print "Listening ..."
+        else:
+            prev_audio.append(cur_data)
 
-wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
-wf.setnchannels(CHANNELS)
-wf.setsampwidth(p.get_sample_size(FORMAT))
-wf.setframerate(RATE)
-wf.writeframes(b''.join(frames))
-wf.close()
+    print "* Done recording"
+    stream.close()
+    p.terminate()
+
+    return response
+
+
+def save_speech(data, p):
+    """ Saves mic data to temporary WAV file. Returns filename of saved 
+        file """
+
+    filename = 'output_'+str(int(time.time()))
+    # writes data to WAV file
+    data = ''.join(data)
+    wf = wave.open(filename + '.wav', 'wb')
+    wf.setnchannels(CHANNELS)
+    wf.setsampwidth(p.get_sample_size(FORMAT))
+    wf.setframerate(RATE)  # TODO make this value a function parameter?
+    wf.writeframes(data)
+    wf.close()
+    return filename + '.wav'
+
+
+if(__name__ == '__main__'):
+    listen_for_speech()  # listen to mic.
